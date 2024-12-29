527ae40e4d53e6ed6e489a8ac695d4d4d787d2ba
Maksim1803 <maks1803@yandex.ru>
2024-12-29 20:43:21 +0300

    Added all project to repository.

---
+++ app/src/main/java/com/example/andr_finalproject_modul_19_part_1/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.andr_finalproject_modul_19_part_1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        }
+    }
---
+++ settings.gradle.kts
@@ -0,0 +1,24 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "ANDR_finalproject_modul_19_part_1"
+include(":app")
+ 
