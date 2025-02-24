32f2bd807e8e6cf66a0627aa7d6780dd6b21f24d
Maksim1803 <maks1803@yandex.ru>
2025-02-24 18:06:18 +0300

    Added new changes to project from module 21.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,6 +7,10 @@ android {
     namespace = "com.example.androidappfilmproject"
     compileSdk = 35
 
+    buildFeatures  {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.example.androidappfilmproject"
         minSdk = 24
