de7c3349ea9e1566c9d0235156720827422ba4d5
Maksim1803 <maks1803@yandex.ru>
2025-08-09 18:39:31 +0300

    Add 09.08.2025 changes to project from module_29 (добавление остальных изменений для переделки SplashActivity в SplashFragment, добавление методов чистки кеша Glide от мусорных картинок, ветка module_29 от develop).

--- app/src/main/java/com/example/androidappfilmproject/MainActivity.kt
+++ app/src/main/java/com/example/androidappfilmproject/MainActivity.kt
@@ -3,7 +3,11 @@ package com.example.androidappfilmproject
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.example.androidappfilmproject.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +16,12 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Очистка кеша Glide (один раз при старте приложения)
+        Glide.get(this).clearMemory()
+        CoroutineScope(Dispatchers.IO).launch {
+            Glide.get(this@MainActivity).clearDiskCache()
+        }
         // Инициализируем binding
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
@@ -23,8 +33,8 @@ class MainActivity : AppCompatActivity() {
             setFragmentBackground(R.drawable.corner_background)
             supportFragmentManager
                 .beginTransaction()
-                .add(it.id, HomeFragment())
-                .addToBackStack(null)
+                .add(it.id, SplashFragment())
+             // .addToBackStack(null)
                 .commit()
         }
     }
