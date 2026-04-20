55cfcef0efe8ad896c818a051553f976d50c74bf
Maks Nikolenko <maks1803@yandex.ru>
2026-04-20 13:11:51 +0300

    Add changes to project from develop (убрал неиспользуемый импорт, ветка develop от master)

--- app/src/pro/java/com/example/androidappfilmproject/MainActivity.kt
+++ app/src/pro/java/com/example/androidappfilmproject/MainActivity.kt
@@ -9,7 +9,6 @@ import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
 import android.view.View
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
