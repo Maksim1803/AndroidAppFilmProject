4a87cdf8233430d3a3be41df5b6ddf6c368d0972
Maks Nikolenko <maks1803@yandex.ru>
2026-07-24 23:26:38 +0300

    Add changes to project (убрал неиспользуемые импорты, 24.07.2026, ветка modul_55 от develop).

--- app/src/main/java/com/example/androidappfilmproject/view/fragments/DetailsFragment.kt
+++ app/src/main/java/com/example/androidappfilmproject/view/fragments/DetailsFragment.kt
@@ -4,8 +4,8 @@ import android.Manifest
 import android.content.ContentValues
 import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
 import android.content.pm.ActivityInfo
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.media.MediaScannerConnection
@@ -17,8 +17,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.webkit.WebChromeClient
-import android.webkit.WebView
-import android.widget.FrameLayout
 import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
